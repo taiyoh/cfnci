9edcd166bf3c93ef16417bcf998ca75f35468830
taiyoh <sun.basix@gmail.com>
2019-04-21 22:27:43 +0900

    add comment and fix typo

--- provider.go
+++ provider.go
@@ -3,16 +3,20 @@ package cfnci
 import (
 	"os"
 
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+	"github.com/aws/aws-sdk-go/service/sqs/sqsiface"
 	"github.com/awslabs/goformation"
 	"github.com/awslabs/goformation/cloudformation"
 	"github.com/awslabs/goformation/intrinsics"
 	"github.com/taiyoh/cfnci/runner"
 )
 
+// Provider represents holding cloudformation template from code.
 type Provider struct {
 	cfTemplate *cloudformation.Template
 }
 
+// New returns Provider object.
 func New(path string, overrides map[string]interface{}) (*Provider, error) {
 	if _, err := os.Stat(path); err != nil {
 		return nil, err
@@ -26,18 +30,20 @@ func New(path string, overrides map[string]interface{}) (*Provider, error) {
 	return &Provider{cft}, nil
 }
 
+// DynamoDB returns runner.DynamoDB object.
 func (p *Provider) DynamoDB(ddb dynamodbiface.DynamoDBAPI, name string) (*runner.DynamoDB, error) {
 	tbl, err := p.cfTemplate.GetAWSDynamoDBTableWithName(name)
 	if err != nil {
 		return nil, err
 	}
-	return runner.NewDynamoDB(ddb, tbl)
+	return runner.NewDynamoDB(ddb, tbl), nil
 }
 
+// SQS returns runner.SQS object.
 func (p *Provider) SQS(svc sqsiface.SQSAPI, endpoint, name string) (*runner.SQS, error) {
 	q, err := p.cfTemplate.GetAWSSQSQueueWithName(name)
 	if err != nil {
 		return nil, err
 	}
-	return runner.NewSQS(endpoint, svc, q)
+	return runner.NewSQS(endpoint, svc, q), nil
 }
